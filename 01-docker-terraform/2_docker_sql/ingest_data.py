97ba420d0421cb07367e5fda49a347e340b39d46
Ringo Yen <ringo.yen31@gmail.com>
2024-05-10 01:34:18 -0700

    updates to ingest_data.py

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -1,33 +1,62 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import argparse
 from time import time
 
 import pandas as pd
 from sqlalchemy import create_engine # need to import so we can create a connection between this notebook and the postgres db
 
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 
-df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000) # allows you to chunk the csv using iterator.
-df = next(df_iter)
+def main(params):
 
-# convert the pickup and dropoff cols to datetime objects
-df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
-df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
 
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace') # just add the columns
+    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 
-
-while True:
-    t_start = time()
-
+    df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000) # allows you to chunk the csv using iterator.
     df = next(df_iter)
 
+    # convert the pickup and dropoff cols to datetime objects
     df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
     df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
 
+    df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace') # just add the columns
+
     df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
 
-    t_end = time()
+    while True:
+        t_start = time()
 
-    print('inserted another chunk, took %.3f second' % (t_end-t_start))
+        df = next(df_iter)
+
+        df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
+        df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('inserted another chunk, took %.3f second' % (t_end-t_start))
+
+
+
+
+
+parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+parser.add_argument('user', help='user name for postgres')
+parser.add_argument('password', help='password for postgres')
+parser.add_argument('host', help='host for postgres')
+parser.add_argument('port', help='port for postgres')
+parser.add_argument('db', help='database name for postgres')
+parser.add_argument('table-name', help='name of the table where we will write the results to')
+parser.add_argument('url', help='url of the csv file')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
