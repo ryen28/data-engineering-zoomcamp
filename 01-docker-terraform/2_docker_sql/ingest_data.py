a2b3b862feb8425afc3187d07af878ea7798b29e
Ringo Yen <ringo.yen31@gmail.com>
2024-05-13 22:34:37 -0700

    updated code

--- 01-docker-terraform/2_docker_sql/ingest_data.py
+++ 01-docker-terraform/2_docker_sql/ingest_data.py
@@ -2,9 +2,11 @@
 # coding: utf-8
 
 import argparse
+import os
 from time import time
 
 import pandas as pd
+import psycopg2
 from sqlalchemy import create_engine # need to import so we can create a connection between this notebook and the postgres db
 
 
@@ -16,19 +18,24 @@ def main(params):
     port = params.port
     db = params.db
     table_name = params.table_name
+    url = params.url
+    csv_name = url.split("/")[-1] # takes the last part of the url file path (the .csv.gz file)
 
-    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    # download the csv file
+    os.system(f"wget {url} -O {csv_name}")
 
-    df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000) # allows you to chunk the csv using iterator.
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000) # allows you to chunk the csv using iterator.
     df = next(df_iter)
 
     # convert the pickup and dropoff cols to datetime objects
     df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
     df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace') # just add the columns
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace') # just add the columns
 
-    df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
 
     while True:
         t_start = time()
@@ -38,7 +45,7 @@ def main(params):
         df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
         df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
 
-        df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
 
         t_end = time()
 
@@ -46,17 +53,18 @@ def main(params):
 
 
 
+if __name__== '__main__':
 
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the csv file')
 
-parser.add_argument('user', help='user name for postgres')
-parser.add_argument('password', help='password for postgres')
-parser.add_argument('host', help='host for postgres')
-parser.add_argument('port', help='port for postgres')
-parser.add_argument('db', help='database name for postgres')
-parser.add_argument('table-name', help='name of the table where we will write the results to')
-parser.add_argument('url', help='url of the csv file')
+    args = parser.parse_args()
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+    main(args)
